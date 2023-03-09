7037480a8c5f437f6cc61b4cbbb12eeb537feff6
edwinkur93 <119643502+edwinkur93@users.noreply.github.com>
2023-03-09 14:54:25 +0900

    Add files via upload

--- Week4-B/Week4-B/Form1.Designer.cs
+++ Week4-B/Week4-B/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Week4_B
 {
-    partial class Form1
+    partial class main_form
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,350 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.check_qst1_1 = new System.Windows.Forms.CheckBox();
+            this.check_qst1_2 = new System.Windows.Forms.CheckBox();
+            this.check_qst1_3 = new System.Windows.Forms.CheckBox();
+            this.check_qst1_4 = new System.Windows.Forms.CheckBox();
+            this.text_qst1_4 = new System.Windows.Forms.TextBox();
+            this.label_qst1 = new System.Windows.Forms.Label();
+            this.check_agree = new System.Windows.Forms.CheckBox();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.label_qst2 = new System.Windows.Forms.Label();
+            this.check_qst2_1 = new System.Windows.Forms.CheckBox();
+            this.check_qst2_2 = new System.Windows.Forms.CheckBox();
+            this.check_qst2_3 = new System.Windows.Forms.CheckBox();
+            this.check_qst2_4 = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.panel_color = new System.Windows.Forms.Panel();
+            this.radio_red = new System.Windows.Forms.RadioButton();
+            this.radio_blue = new System.Windows.Forms.RadioButton();
+            this.radio_green = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.panel_color.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // check_qst1_1
+            // 
+            this.check_qst1_1.AutoSize = true;
+            this.check_qst1_1.Location = new System.Drawing.Point(47, 67);
+            this.check_qst1_1.Name = "check_qst1_1";
+            this.check_qst1_1.Size = new System.Drawing.Size(72, 20);
+            this.check_qst1_1.TabIndex = 0;
+            this.check_qst1_1.Text = "Teman";
+            this.check_qst1_1.UseVisualStyleBackColor = true;
+            // 
+            // check_qst1_2
+            // 
+            this.check_qst1_2.AutoSize = true;
+            this.check_qst1_2.Location = new System.Drawing.Point(47, 94);
+            this.check_qst1_2.Name = "check_qst1_2";
+            this.check_qst1_2.Size = new System.Drawing.Size(108, 20);
+            this.check_qst1_2.TabIndex = 1;
+            this.check_qst1_2.Text = "Social Media";
+            this.check_qst1_2.UseVisualStyleBackColor = true;
+            // 
+            // check_qst1_3
+            // 
+            this.check_qst1_3.AutoSize = true;
+            this.check_qst1_3.Location = new System.Drawing.Point(47, 121);
+            this.check_qst1_3.Name = "check_qst1_3";
+            this.check_qst1_3.Size = new System.Drawing.Size(57, 20);
+            this.check_qst1_3.TabIndex = 2;
+            this.check_qst1_3.Text = "Guru";
+            this.check_qst1_3.UseVisualStyleBackColor = true;
+            // 
+            // check_qst1_4
+            // 
+            this.check_qst1_4.AutoSize = true;
+            this.check_qst1_4.Location = new System.Drawing.Point(47, 147);
+            this.check_qst1_4.Name = "check_qst1_4";
+            this.check_qst1_4.Size = new System.Drawing.Size(18, 17);
+            this.check_qst1_4.TabIndex = 3;
+            this.check_qst1_4.UseVisualStyleBackColor = true;
+            this.check_qst1_4.CheckedChanged += new System.EventHandler(this.check_qst1_4_CheckedChanged);
+            // 
+            // text_qst1_4
+            // 
+            this.text_qst1_4.Enabled = false;
+            this.text_qst1_4.Location = new System.Drawing.Point(71, 147);
+            this.text_qst1_4.Name = "text_qst1_4";
+            this.text_qst1_4.Size = new System.Drawing.Size(100, 22);
+            this.text_qst1_4.TabIndex = 4;
+            this.text_qst1_4.Text = "others . . .";
+            // 
+            // label_qst1
+            // 
+            this.label_qst1.AutoSize = true;
+            this.label_qst1.Location = new System.Drawing.Point(68, 29);
+            this.label_qst1.Name = "label_qst1";
+            this.label_qst1.Size = new System.Drawing.Size(169, 16);
+            this.label_qst1.TabIndex = 5;
+            this.label_qst1.Text = "Mengetahui UC dari mana?";
+            // 
+            // check_agree
+            // 
+            this.check_agree.AutoSize = true;
+            this.check_agree.Location = new System.Drawing.Point(47, 200);
+            this.check_agree.Name = "check_agree";
+            this.check_agree.Size = new System.Drawing.Size(211, 20);
+            this.check_agree.TabIndex = 6;
+            this.check_agree.Text = "I agree to the Terms of Service";
+            this.check_agree.UseVisualStyleBackColor = true;
+            this.check_agree.CheckedChanged += new System.EventHandler(this.check_agree_CheckedChanged);
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.Enabled = false;
+            this.btn_submit.Location = new System.Drawing.Point(123, 226);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(75, 23);
+            this.btn_submit.TabIndex = 7;
+            this.btn_submit.Text = "Submit";
+            this.btn_submit.UseVisualStyleBackColor = true;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
+            // 
+            // label_qst2
+            // 
+            this.label_qst2.AutoSize = true;
+            this.label_qst2.Location = new System.Drawing.Point(47, 296);
+            this.label_qst2.Name = "label_qst2";
+            this.label_qst2.Size = new System.Drawing.Size(156, 16);
+            this.label_qst2.TabIndex = 8;
+            this.label_qst2.Text = "Pilih 2 Makanan Favorite:";
+            // 
+            // check_qst2_1
+            // 
+            this.check_qst2_1.AutoSize = true;
+            this.check_qst2_1.Location = new System.Drawing.Point(47, 327);
+            this.check_qst2_1.Name = "check_qst2_1";
+            this.check_qst2_1.Size = new System.Drawing.Size(105, 20);
+            this.check_qst2_1.TabIndex = 9;
+            this.check_qst2_1.Text = "Nasi Goreng";
+            this.check_qst2_1.UseVisualStyleBackColor = true;
+            this.check_qst2_1.CheckedChanged += new System.EventHandler(this.check_qst2_1_CheckedChanged);
+            // 
+            // check_qst2_2
+            // 
+            this.check_qst2_2.AutoSize = true;
+            this.check_qst2_2.Location = new System.Drawing.Point(47, 354);
+            this.check_qst2_2.Name = "check_qst2_2";
+            this.check_qst2_2.Size = new System.Drawing.Size(99, 20);
+            this.check_qst2_2.TabIndex = 10;
+            this.check_qst2_2.Text = "Mie Goreng";
+            this.check_qst2_2.UseVisualStyleBackColor = true;
+            this.check_qst2_2.CheckedChanged += new System.EventHandler(this.check_qst2_2_CheckedChanged);
+            // 
+            // check_qst2_3
+            // 
+            this.check_qst2_3.AutoSize = true;
+            this.check_qst2_3.Location = new System.Drawing.Point(47, 381);
+            this.check_qst2_3.Name = "check_qst2_3";
+            this.check_qst2_3.Size = new System.Drawing.Size(68, 20);
+            this.check_qst2_3.TabIndex = 11;
+            this.check_qst2_3.Text = "Bakso";
+            this.check_qst2_3.UseVisualStyleBackColor = true;
+            this.check_qst2_3.CheckedChanged += new System.EventHandler(this.check_qst2_3_CheckedChanged);
+            // 
+            // check_qst2_4
+            // 
+            this.check_qst2_4.AutoSize = true;
+            this.check_qst2_4.Location = new System.Drawing.Point(47, 408);
+            this.check_qst2_4.Name = "check_qst2_4";
+            this.check_qst2_4.Size = new System.Drawing.Size(107, 20);
+            this.check_qst2_4.TabIndex = 12;
+            this.check_qst2_4.Text = "Nasi Campur";
+            this.check_qst2_4.UseVisualStyleBackColor = true;
+            this.check_qst2_4.CheckedChanged += new System.EventHandler(this.check_qst2_4_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(405, 67);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 20);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(405, 93);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(103, 20);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(405, 120);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(103, 20);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // panel_color
+            // 
+            this.panel_color.Controls.Add(this.radio_green);
+            this.panel_color.Controls.Add(this.radio_blue);
+            this.panel_color.Controls.Add(this.radio_red);
+            this.panel_color.Location = new System.Drawing.Point(405, 179);
+            this.panel_color.Name = "panel_color";
+            this.panel_color.Size = new System.Drawing.Size(200, 100);
+            this.panel_color.TabIndex = 19;
+            // 
+            // radio_red
+            // 
+            this.radio_red.AutoSize = true;
+            this.radio_red.Location = new System.Drawing.Point(4, 4);
+            this.radio_red.Name = "radio_red";
+            this.radio_red.Size = new System.Drawing.Size(54, 20);
+            this.radio_red.TabIndex = 0;
+            this.radio_red.TabStop = true;
+            this.radio_red.Text = "Red";
+            this.radio_red.UseVisualStyleBackColor = true;
+            this.radio_red.CheckedChanged += new System.EventHandler(this.radio_red_CheckedChanged);
+            // 
+            // radio_blue
+            // 
+            this.radio_blue.AutoSize = true;
+            this.radio_blue.Location = new System.Drawing.Point(4, 31);
+            this.radio_blue.Name = "radio_blue";
+            this.radio_blue.Size = new System.Drawing.Size(55, 20);
+            this.radio_blue.TabIndex = 1;
+            this.radio_blue.TabStop = true;
+            this.radio_blue.Text = "Blue";
+            this.radio_blue.UseVisualStyleBackColor = true;
+            this.radio_blue.CheckedChanged += new System.EventHandler(this.radio_blue_CheckedChanged);
+            // 
+            // radio_green
+            // 
+            this.radio_green.AutoSize = true;
+            this.radio_green.Location = new System.Drawing.Point(4, 58);
+            this.radio_green.Name = "radio_green";
+            this.radio_green.Size = new System.Drawing.Size(63, 20);
+            this.radio_green.TabIndex = 2;
+            this.radio_green.TabStop = true;
+            this.radio_green.Text = "green";
+            this.radio_green.UseVisualStyleBackColor = true;
+            this.radio_green.CheckedChanged += new System.EventHandler(this.radio_green_CheckedChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.radioButton9);
+            this.panel2.Controls.Add(this.radioButton8);
+            this.panel2.Controls.Add(this.radioButton7);
+            this.panel2.Location = new System.Drawing.Point(405, 301);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 20;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(4, 4);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(103, 20);
+            this.radioButton7.TabIndex = 0;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "radioButton7";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(4, 31);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(103, 20);
+            this.radioButton8.TabIndex = 1;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "radioButton8";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(4, 58);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(103, 20);
+            this.radioButton9.TabIndex = 2;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "radioButton9";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // main_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel_color);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.check_qst2_4);
+            this.Controls.Add(this.check_qst2_3);
+            this.Controls.Add(this.check_qst2_2);
+            this.Controls.Add(this.check_qst2_1);
+            this.Controls.Add(this.label_qst2);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.check_agree);
+            this.Controls.Add(this.label_qst1);
+            this.Controls.Add(this.text_qst1_4);
+            this.Controls.Add(this.check_qst1_4);
+            this.Controls.Add(this.check_qst1_3);
+            this.Controls.Add(this.check_qst1_2);
+            this.Controls.Add(this.check_qst1_1);
+            this.Name = "main_form";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel_color.ResumeLayout(false);
+            this.panel_color.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox check_qst1_1;
+        private System.Windows.Forms.CheckBox check_qst1_2;
+        private System.Windows.Forms.CheckBox check_qst1_3;
+        private System.Windows.Forms.CheckBox check_qst1_4;
+        private System.Windows.Forms.TextBox text_qst1_4;
+        private System.Windows.Forms.Label label_qst1;
+        private System.Windows.Forms.CheckBox check_agree;
+        private System.Windows.Forms.Button btn_submit;
+        private System.Windows.Forms.Label label_qst2;
+        private System.Windows.Forms.CheckBox check_qst2_1;
+        private System.Windows.Forms.CheckBox check_qst2_2;
+        private System.Windows.Forms.CheckBox check_qst2_3;
+        private System.Windows.Forms.CheckBox check_qst2_4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Panel panel_color;
+        private System.Windows.Forms.RadioButton radio_green;
+        private System.Windows.Forms.RadioButton radio_blue;
+        private System.Windows.Forms.RadioButton radio_red;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton7;
     }
 }
 
--- Week4-B/Week4-B/Form1.cs
+++ Week4-B/Week4-B/Form1.cs
@@ -10,11 +10,237 @@ using System.Windows.Forms;
 
 namespace Week4_B
 {
-    public partial class Form1 : Form
+    public partial class main_form : Form
     {
-        public Form1()
+        public main_form()
         {
             InitializeComponent();
+        }
+
+        private int count = 0;
+
+        private void check_qst1_4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (check_qst1_4.Checked)
+            {
+                text_qst1_4.Enabled = true;
+                text_qst1_4.Text = "";
+            }
+            else
+            {
+                text_qst1_4.Enabled = false;
+                text_qst1_4.Text = "others . . .";
+            }
+        }
+
+        private void check_agree_CheckedChanged(object sender, EventArgs e)
+        {
+            if (check_agree.Checked)
+            {
+                btn_submit.Enabled = true;
+            }
+            else
+            {
+                btn_submit.Enabled = false;
+            }
+        }
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            string text = "Saya tahu UC dari ";
+            if (check_qst1_1.Checked == false && check_qst1_2.Checked == false &&
+                check_qst1_3.Checked == false && check_qst1_4.Checked == false)
+            {
+                string errorText = "Minimal 1 Checkbox harus di Centang";
+                MessageBox.Show(errorText, "Checkbox Kosong",
+                    MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                if (check_qst1_1.Checked)
+                {
+                    text += "Teman, ";
+                }
+                if (check_qst1_2.Checked)
+                {
+                    text += "Social Media, ";
+                }
+                if (check_qst1_3.Checked)
+                {
+                    text += "Guru ";
+                }
+                if (check_qst1_4.Checked)
+                {
+                    text += text_qst1_4.Text;
+                }
+                MessageBox.Show(text, "Saya Tahu UC",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void check_qst2_1_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (check_qst2_1.Checked)
+            {
+
+                count++;
+                Console.WriteLine(count);
+            }
+            else
+            {
+                count--;
+                Console.WriteLine(count);
+            }
+            if (count == 2)
+            {
+                if (check_qst2_2.Checked)
+                {
+                    check_qst2_3.Enabled = false;
+                    check_qst2_4.Enabled = false;
+                }
+                else if (check_qst2_3.Checked)
+                {
+                    check_qst2_2.Enabled = false;
+                    check_qst2_4.Enabled = false;
+                }
+                else if (check_qst2_4.Checked)
+                {
+                    check_qst2_2.Enabled = false;
+                    check_qst2_3.Enabled = false;
+                }
+            }
+            checkChoice();
+        }
+        private void radio_green_CheckedChanged(object sender, EventArgs e)
+        {
+            panel_color.BackColor = Color.Green;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radio_red_CheckedChanged(object sender, EventArgs e)
+        {
+            panel_color.BackColor = Color.Red;
+            main_form.ActiveForm.BackColor = Color.Khaki;
+        }
+
+        private void radio_blue_CheckedChanged(object sender, EventArgs e)
+        {
+            panel_color.BackColor = Color.Blue;
+        }
+
+        private void checkChoice()
+        {
+            if (count < 2)
+            {
+                check_qst2_1.Enabled = true;
+                check_qst2_2.Enabled = true;
+                check_qst2_3.Enabled = true;
+                check_qst2_4.Enabled = true;
+            }
+        }
+
+
+
+        private void check_qst2_2_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (check_qst2_2.Checked)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+            if (count == 2)
+            {
+                if (check_qst2_1.Checked)
+                {
+                    check_qst2_3.Enabled = false;
+                    check_qst2_4.Enabled = false;
+                }
+                else if (check_qst2_3.Checked)
+                {
+                    check_qst2_1.Enabled = false;
+                    check_qst2_4.Enabled = false;
+                }
+                else if (check_qst2_4.Checked)
+                {
+                    check_qst2_1.Enabled = false;
+                    check_qst2_3.Enabled = false;
+                }
+                
+            }
+            checkChoice();
+        }
+
+        private void check_qst2_3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (check_qst2_3.Checked)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+            if (count == 2)
+            {
+                if (check_qst2_1.Checked)
+                {
+                    check_qst2_2.Enabled = false;
+                    check_qst2_4.Enabled = false;
+                }
+                else if (check_qst2_2.Checked)
+                {
+                    check_qst2_1.Enabled = false;
+                    check_qst2_4.Enabled = false;
+                }
+                else if (check_qst2_4.Checked)
+                {
+                    check_qst2_1.Enabled = false;
+                    check_qst2_2.Enabled = false;
+                }
+                
+            }
+            checkChoice();
+        }
+
+        private void check_qst2_4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (check_qst2_4.Checked)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+            if (count == 2)
+            {
+                if (check_qst2_1.Checked)
+                {
+                    check_qst2_2.Enabled = false;
+                    check_qst2_3.Enabled = false;
+                }
+                else if (check_qst2_2.Checked)
+                {
+                    check_qst2_1.Enabled = false;
+                    check_qst2_3.Enabled = false;
+                }
+                else if (check_qst2_3.Checked)
+                {
+                    check_qst2_1.Enabled = false;
+                    check_qst2_2.Enabled = false;
+                }
+                
+            }
+            checkChoice();
         }
     }
 }
